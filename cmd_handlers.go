185ac0630955ac6cbde70e91a99545b17d8e2631
Dylan Kubic <dylanjkubic@gmail.com>
2024-11-26 10:17:00 -0500

    updating register and run methods

--- cmd_handlers.go
+++ cmd_handlers.go
@@ -1,24 +1,48 @@
 package main
 
-import gatorconfig "github.com/ankylosaurus11/blog_agg/internal/config"
+import (
+	"errors"
+	"fmt"
+
+	gatorconfig "github.com/ankylosaurus11/blog_agg/internal/config"
+)
 
 type state struct {
 	ConfigPointer *gatorconfig.Config
 }
 
 type command struct {
-	Name     string
+	Name    string
 	Command []string
 }
 
 type commands struct {
-	
+	Cmd map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.Cmd[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.Cmd[cmd.Name]; ok {
+		err := handler(s, cmd)
+		if err != nil {
+			fmt.Println("Error executing command:", err)
+			return err
+		} else {
+			return fmt.Errorf("command not found: %s", cmd.Name)
+		}
+	return nil
+	}
 }
 
 func handlerLogin(s *state, cmd command) error {
-	if cmd.Command = []string{} {
-		errors.New("not enough arguments were provided")
+	if len(cmd.Command) == 0 {
+		return errors.New("not enough arguments were provided")
 	}
-	s.ConfigPointer.Config.CurrentUserName = cmd.Command[1]
-	fmt.SPrintf("user: %v, has been set.", cmd.Command[1])
+	s.ConfigPointer.CurrentUserName = cmd.Command[0]
+	fmt.Println("set user:", cmd.Command[0])
+
+	return nil
 }
